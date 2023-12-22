7ae31e49a91b795b1477aa95856e47f1392a4276
quole300903 <quocle300903@gmail.com>
2023-12-22 17:11:23 +0700

    done CRUD

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,31 +7,33 @@
         <div class="row justify-content-center">
             <table class="table table-striped">
                 <thead>
-                <tr>
+                <tr class="text-center">
                     <th>ID</th>
                     <th>Name</th>
-                    <th style="width: 1000px">Content</th>
+                    <th>Content</th>
                     <th>Actions</th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($tasks as $task)
-                    <tr>
-                        <td>{{ $task->id }}</td>
+                    <tr class="align-middle">
+                        <td class="text-center">{{ $task->id }}</td>
                         <td>{{ $task->name }}</td>
                         <td style="height: 80px;width: 70%">{{ $task->content }}</td>
-                        <td>
+                        <td class="text-center">
                             <form id="delete-form-{{ $task->id }}" method="POST" action="{{ route('tasks.destroy', $task->id) }}">
                                 @csrf
                                 @method('DELETE')
                                 <button type="button" class="btn btn-danger" onclick="confirmDelete('{{ $task->id }}')">Delete</button>
-                                <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-warning">Edit</a>
+                                <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-warning" style="margin-left: 3px">Edit</a>
                             </form>
                         </td>
                     </tr>
                 @endforeach
                 </tbody>
             </table>
+        </div>
+        <div class="row justify-content-center">
             {{ $tasks->links() }}
         </div>
     </div>
